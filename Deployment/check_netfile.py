a0fb1127b2dd75768e49973778e962331d3e37f9
mata.hou <15910963100@163.com>
2015-04-08 10:36:10 +0800

    asss

--- Deployment/check_netfile.py
+++ Deployment/check_netfile.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python
 import os
+import ConfigParser
+
+config=ConfigParser.ConfigParser()
+config.readfp(open('config.ini',"rb"))
+int=config.get("global","NetInterface")
+
+file_path='/etc/sysconfig/network-scripts/'
+
+def  isfile():
+    file=os.path.exists(file_path+int)
+    return file
+
+isfile()
