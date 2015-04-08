ba8de5af13ea44155a9463e6277b22fc857cf9f4
mata.hou <15910963100@163.com>
2015-04-08 17:23:56 +0800

    asss

--- Deployment/update_ip.py
+++ Deployment/update_ip.py
@@ -13,24 +13,62 @@ dns1=config.get("global",'dns1')
 dns2=config.get("global",'dns2')
 path='/etc/sysconfig/network-scripts/'
 
-a=open(path+NetInterface,'r')
-match=re.compile(r'ip')
-match1=re.compile(r'dns1')
-list=[]
-while 1:
-    c= a.readline()
-    if not c:
-        break
-    elif match.search(c):
-        pass
-    elif match1.search(c):
-        pass
-    else:
-        list.append(c)
-a.close()
-list.append("ip = 192.168.12.123")
-a=open('config.ini','w')
-for i in list:
-    a.write(i)
-a.close()
+def onboot():
+    a=open(path+NetInterface,'r')
+    match=re.compile(r'ONBOOT')
+    list=[]
+        while 1:
+        c= a.readline()
+        if not c:
+            break
+        elif match.search(c):
+            pass
+        else:
+            list.append(c)
+    a.close()
+    list.append("ONBOOT=yes")
+    a=open('path+NetInterface','w')
+        for i in list:
+        a.write(i)
+    a.close()
+
+def ip():
+    a=open(path+NetInterface,'r')
+    match_bootproto=re.compile(r'BOOTPROTO')
+    match_ip=re.compile(r'IPADDR')
+    match_netmask=re.compile(r'NETMASK')
+    match_gateway=re.compile(r'GATEWAY')
+    match_dns1=re.compile(r'DNS1')
+    match_dns2=re.compile(r'DNS2')
+    list=[]
+        while 1:
+            c= a.readline()
+            if not c:
+                break
+            elif match_bootproto.search(c):
+                pass
+            elif match_ip.search(c):
+                pass
+            elif match_netmask.search(c):
+                pass
+            elif match_gatewayo.search(c):
+                pass
+            elif match_dns1.search(c):
+                pass
+            elif match_dns2.search(c):
+                pass
+            else:
+                list.append(c)
+    a.close()
+    list.append("BOOTPROTO= static")
+    list.append("IPADDR=%s"%ip)
+    list.append("NETMASK=%s"%netmask)
+    list.append("GATEWAY=%s"%gateway)
+    list.append("DNS1=%s"%dns1)
+    list.append("DNS2=%s"%dns2)
+    a=open('path+NetInterface','w')
+        for i in list:
+        a.write(i)
+    a.close()
+
 
