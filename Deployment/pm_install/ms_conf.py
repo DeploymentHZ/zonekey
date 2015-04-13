bcf0a508c8dfc83fb2ce9234de3930f36ad87f7d
mata.hou <15910963100@163.com>
2015-04-13 16:51:32 +0800

    q

--- Deployment/pm_install/ms_conf.py
+++ Deployment/pm_install/ms_conf.py
@@ -4,23 +4,26 @@ import ConfigParser
 config=ConfigParser.ConfigParser()
 config.readfp(open('./conf/config.ini',"rb"))
 ip=config.get("global",'ip')
-a=open('global_conf.json','r')\
-b=a.readline()
+a=open('global_conf.json','r')
+
 def mod_ip ():
     ip1 = re.compile(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])')
+    list=[]
     while 1:
+        b=a.readline()
+        c=ip1.search(b)
+        list.append(b)
         if not b:
             break
-        elif ip1.search(b):
-            print 1111
+        elif c:
+            add= list.index(b)
+            del list[add]
+            w=re.sub(ip1,ip,b)
+            list.insert(add,w)
 
-
-    a.close(
-
-    )
-
-
-
-
-
+    a.close()
+    aa=open('global_conf.json','w')
+    for i in list:
+        aa.write(i)
+    aa.close()
 mod_ip()
