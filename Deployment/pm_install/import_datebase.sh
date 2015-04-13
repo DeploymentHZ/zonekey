811c9460fce7be5c5cc6ba284dc9fe13d355dde0
mata.hou <15910963100@163.com>
2015-04-13 17:27:36 +0800

    q

--- Deployment/pm_install/import_datebase.sh
+++ Deployment/pm_install/import_datebase.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 name=`ls  ./data |grep *.sql|cut -d . -f 1`
-mysqladmin -uroot password $name
-mysql -u root -p$name  < ./data/$name.sql
+echo $name
+#mysqladmin -uroot password $name
+#mysql -u root -p$name  < ./data/$name.sql
