a1992282ccfc7aaef051149c6dba2d87741dcb67
mata.hou <15910963100@163.com>
2015-04-14 11:11:49 +0800

    q

--- Deployment/__init__.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'hou'
-#!/bin/bash
-filename=`find /home/soft -name *.sql|awk -F "/" '{print $4}'|awk -F "." '{print $1}'`
-mysqladmin -uroot password $filename
-mysql -u root -p $filename < /home/soft/$filename.sql
