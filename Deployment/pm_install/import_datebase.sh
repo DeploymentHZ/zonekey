f76f17a1b0eea82efb3094456faeec583e370630
mata.hou <15910963100@163.com>
2015-04-14 10:01:29 +0800

    q

--- Deployment/pm_install/import_datebase.sh
+++ Deployment/pm_install/import_datebase.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-name=`ls  ./data |grep *.sql|cut -d . -f 1`
-echo $name
-#mysqladmin -uroot password $name
-#mysql -u root -p$name  < ./data/$name.sql
+dbname=`cat  ./conf/config.ini |grep dbname|cut -d = -f 2`
+dbfile=`cat  ./conf/config.ini |grep dbfile|cut -d = -f 2`
+dbpwd=`cat  ./conf/config.ini |grep dbpwd|cut -d = -f 2`
+
+mysqladmin -uroot password $dbpwd
+mysql -uroot -p$dbpwd -e "create database $dbname DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"
+mysql -u root -p$dbname  < ./data/$dbfile.sql
